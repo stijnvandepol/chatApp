130c06facebbd5926150ee60439ef4aa0be10ab0
Stijn0vp <Stijnvdpol0@gmail.com>
2024-04-24 12:59:32 +0200

    nee

--- src/persistence/mysql.js
+++ src/persistence/mysql.js
@@ -1,6 +1,6 @@
 const waitPort = require('wait-port');
 const fs = require('fs');
-const mysql = require('mysql2');
+const mysql = require('mysql');
 
 const {
     MYSQL_HOST: HOST,
@@ -21,12 +21,7 @@ async function init() {
     const password = PASSWORD_FILE ? fs.readFileSync(PASSWORD_FILE) : PASSWORD;
     const database = DB_FILE ? fs.readFileSync(DB_FILE) : DB;
 
-    await waitPort({ 
-        host, 
-        port: 3306,
-        timeout: 10000,
-        waitForDns: true,
-    });
+    await waitPort({ host, port : 3306});
 
     pool = mysql.createPool({
         connectionLimit: 5,
